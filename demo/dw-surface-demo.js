921849f6eb8c725db90e83d49ea4a1c11fcd3ed8
shakil_mansuri_dream <shakil2796>
2020-07-25 14:54:28 +0530

    feat: use our lit element instead of lit-element base class.

--- demo/dw-surface-demo.js
+++ demo/dw-surface-demo.js
@@ -8,7 +8,8 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { LitElement, html, css} from 'lit-element';
+import { html, css} from 'lit-element';
+import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
 import '../dw-surface.js';
 import { ThemeStyle } from '@dreamworld/material-styles/theme';
 import '@material/mwc-switch';
