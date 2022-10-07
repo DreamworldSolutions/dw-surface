db26be808e047dff679811a651daffb14a2cdfcb
Ujjval Kukadiya <ujjval@dreamworld.dev>
2022-10-07 12:28:33 +0530

    feat: upgrade to latest lit version.
    BREAKING CHANGE: https://a.kerika.net/orI/board/VuCl/Zo_8
    
    fix: update config.yml
    
    fix: typo in config file
    
    fix: update done.
    
    fix: update dw-ripple and material-styles version

--- demo/dw-surface-demo.js
+++ demo/dw-surface-demo.js
@@ -1,15 +1,4 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
-import { html, css} from 'lit-element';
-import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
+import { LitElement, html, css } from '@dreamworld/pwa-helpers/lit.js';
 import '../dw-surface.js';
 import { ThemeStyle } from '@dreamworld/material-styles/theme';
 import '@material/mwc-switch';
@@ -93,8 +82,8 @@ class DwSurfaceDemo extends LitElement {
     
     return html`
       <mwc-formfield label="Enable dark theme">
-         <mwc-switch @change="${(e) => {
-            if (e.target.checked) { 
+         <mwc-switch @click="${(e) => {
+            if (e.target.selected) { 
               this.setAttribute('dark-theme', e.detail);
               return;
             }
